1f06eced3cb74ef1aabd2008986c62808920aa42
Chernakov Ilya <chernakov.ilya@gmail.com>
2022-09-16 16:07:18 +0300

    finished lesson

---
+++ Task05/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число ");
+int numA = Convert.ToInt32(Console.ReadLine());
+
+int numMinusA = numA * (-1);
+
+while (numMunusA <= numA)
+{
+ Console.Write($"{numMunusA}, ");
+ numMunusA++;
+}
