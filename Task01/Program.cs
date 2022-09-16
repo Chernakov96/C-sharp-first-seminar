ec20b3e43c81b41e714de2ddad2e072d534e35d5
Chernakov Ilya <chernakov.ilya@gmail.com>
2022-09-16 16:07:56 +0300

    all this

---
+++ Task01/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.Write("Введите два целых числа ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите два целых числа ");
+int square = Convert.ToInt32(Console.ReadLine());
---
+++ Task03/Program.cs
@@ -0,0 +1,62 @@
+﻿Console.WriteLine("Введите число от 1 до 7");
+int num = Convert.ToInt32(Console.ReadLine());
+if(num > 7)
+{
+    Console.WriteLine("Вы ввели некорректное число");
+}
+if(num == 1)
+{
+    Console.WriteLine("пн");
+}
+if(num == 2)
+{
+    Console.WriteLine("вт");
+}
+if(num == 3)
+{
+    Console.WriteLine("ср");
+}
+if(num == 4)
+{
+    Console.WriteLine("чт");
+}
+if(num == 5)
+{
+    Console.WriteLine("пт");
+}
+if(num == 6)
+{
+    Console.WriteLine("сб");
+}
+if(num == 7)
+{
+    Console.WriteLine("вс");
+}
+
+
+switch(num)
+{
+    case 1:
+    Console.WriteLine("пн");
+    break;
+    case 2:
+    Console.WriteLine("вт");
+    break;
+    case 3:
+    Console.WriteLine("ср");
+    break;
+    case 4:
+    Console.WriteLine("чт");
+    break;
+    case 5:
+    Console.WriteLine("пт");
+    break;
+    case 6:
+    Console.WriteLine("сб");
+    break;
+    case 7:
+    Console.WriteLine("вс");
+    break;
+    default:
+        Console.WriteLine()
+}
