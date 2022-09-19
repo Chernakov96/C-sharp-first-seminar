867aff34aa782712523edf37c122407270728fee
Chernakov Ilya <chernakov.ilya@gmail.com>
2022-09-19 21:45:46 +0300

    tried task 012

--- Task011/Program.cs
+++ Task011/Program.cs
@@ -12,8 +12,7 @@ Console.WriteLine($"{rNumberum}");
 int DeleteSecondDigit(int num)
 {
     int firstDigit = num / 100;
-    int secondDigit = (num - 100 * firstDigit) / 10;
-    int thirdDigit = (num - 100 * firstDigit - secondDigit * 10);
+    int thirdDigit = num % 10;
     int deletedSecondDigit = firstDigit * 10 + thirdDigit;
     return deletedSecondDigit;
 }
--- Task012/Program.cs
+++ Task012/Program.cs
@@ -3,5 +3,18 @@
 // 2.выводить, является ли второе число кратным первому
 // 3. Если число 2 не кратно числу 1, то программа выводит остаток от деления.
 
+Console.Write("Введите первое число ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число ");
+int num2 = Convert.ToInt32(Console.ReadLine());
 
+string result = num1 % num2 == 0 ? Console.WriteLine($"Число {num1} кратно числу {num2}") : Console.WriteLine($"Число {num1}  не кратно числу {num2} остаток от деления = {num1%num2}");
 
+if ((numA % numB) == 0)
+{
+    Console.WriteLine($"Число {numA} кратно числу {numB}");
+}
+else
+{ 
+    Console.WriteLine($"Число {numA} не кратно числу {numB}, остаток от деления {numA % numB}"); 
+}
