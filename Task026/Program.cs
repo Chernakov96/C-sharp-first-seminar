a1ffb7a22da6d588f3dd8ee6b9b12cf1708a5e89
Илья Чернаков <ichernakov@2A.local>
2022-09-26 21:07:38 +0300

    add task 024 & task 026

---
+++ Task026/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 26: Напишите программу, которая 
+// 1.принимает на вход число и 
+// 2. выдаёт количество цифр в числе.
+// Пример:
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+System.Console.WriteLine("Введите число ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int sumOfDigits(int num)
+{
+    int count = 0;
+    while(num != 0)
+    {
+        num = num /10;
+        count++;
+    }
+    return count;
+}
+int result = sumOfDigits(number);
+System.Console.WriteLine($"{result}");
---
+++ task024/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 24: Напишите программу, которая
+// 1. принимает на вход число (А) и 
+// 2.выдаёт сумму чисел от 1 до А.
+// Пример:
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+System.Console.WriteLine("Введите число ");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = SumNumbers(number);
+System.Console.WriteLine($"Сумма чисел от 1 до {number} = {result}");
+
+// int SumNumbers(int num)
+// {
+//     int sum = default;
+//     for (int i = 1; i <= num; i++)
+//     {
+//         sum = sum + i;
+//     }
+//     return sum;
+// }
+
+int SumNumbers(int num)
+{
+    int sum = default;
+    int count = 1;
+    while (count <= num)
+    {
+       sum += count;
+       count++; 
+    }
+    return sum;
+}
