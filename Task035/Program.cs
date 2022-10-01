8daf563c4bb289fd0f4e1319bd913c2afeac9925
Илья Чернаков <ichernakov@2A.local>
2022-10-01 18:06:10 +0300

    task 035 complete

---
+++ Task035/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задача 35: 
+// 1. Задайте одномерный массив из 123 случайных чисел.
+// 2. Найдите количество элементов массива, значения которых лежат в отрезке [10,99].
+// Пример для массива из 5, а не 123 элементов. В своём решении сделайте для 123
+// [5, 18, 123, 6, 2] -> 1
+// [1, 2, 3, 6, 2] -> 0
+// [10, 11, 12, 13, 14] -> 5
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] array = new int[size];
+    var rnd = new Random();
+
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(min, max + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+int[] arr = CreateArrayRndInt(123, 1, 150);
+PrintArray(arr);
+
+int SumOfNumbersInInterval(int[] array)
+{
+    int sum = default;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i] / 10 >= 1 && array[i] / 10 < 10)
+        sum++;
+    }
+    return sum;
+}
+int result = SumOfNumbersInInterval(arr);
+Console.WriteLine($"Количество числе в промежутке 10-99 = {result}");
