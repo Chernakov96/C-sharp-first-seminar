d79d43f58531388bf381ae4745910dc39a6cfbf6
Илья Чернаков <ichernakov@2A.local>
2022-10-01 17:36:18 +0300

    task 031, task 032 done

---
+++ Task031/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 32: Напишите программу замена элементов
+// массива:
+// 1.Создать массив
+// 2.Вывести массив  
+// 3. положительные элементы замените насоответствующие отрицательные, и наоборот.
+// Пример:
+// [-4, -8, 8, 2] -> [4, 8, -8, -2]
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] array = new int[size];
+    var rnd = new Random();
+
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(min, max + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.WriteLine("]");
+}
+void InveverseArrayElems(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = array[i] * -1;
+    }
+}
+
+int[] arr = CreateArrayRndInt(4, -10, 10);
+PrintArray(arr);
+InveverseArrayElems(arr);
+PrintArray(arr);
