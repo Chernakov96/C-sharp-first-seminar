c5f6c43121e99fa9e5536d22eb332d8113a71abb
Илья Чернаков <ichernakov@2A.local>
2022-10-07 18:30:20 +0300

    started 049 and 051

---
+++ Task048/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 48: 
+// 1. Задайте двумерный массив размера m на n,
+// 2. каждый элемент в массиве находится по формуле: Amn = m+n.
+// 3. Выведите полученный массив на экран.
+// Пример:
+// m = 3, n = 4.
+// 0 1 2 3
+// 1 2 3 4
+// 2 3 4 5
+
+int[,] CreateMatrixSumIndex(int rows, int columns)
+{
+    int[,] matrix = new int[rows, columns];
+
+    for (int i = 0; i < rows/*i < matrix.GetLength(0)*/; i++) //0 rows
+    {
+        for (int j = 0; j < columns/*i < matrix.GetLength(1)*/; j++) //1 columns
+        {
+            matrix[i, j] = i+j;
+        }
+    }
+
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i,j], 4}, ");
+            else Console.Write($"{matrix[i,j], 4}");
+        }
+        Console.WriteLine("]");
+    }
+}
+
+int[,] array2D = CreateMatrixSumIndex(3,4);
+PrintMatrix(array2D);
