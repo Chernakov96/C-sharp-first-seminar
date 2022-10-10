6e63328d5077d73353cc3405b5629d885abfa503
Илья Чернаков <ichernakov@2A.local>
2022-10-10 20:37:41 +0300

    task 053 finished

---
+++ Task053/Program.cs
@@ -0,0 +1,53 @@
+﻿// Задача 53: 
+// 1. Задайте двумерный массив. Напишите программу, которая 
+// 2. поменяет местами первую и последнюю строку массива.
+
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    var rnd = new Random();
+
+    for (int i = 0; i < rows/*i < matrix.GetLength(0)*/; i++) //0 rows
+    {
+        for (int j = 0; j < columns/*i < matrix.GetLength(1)*/; j++) //1 columns
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],4}, ");
+            else Console.Write($"{matrix[i, j],4}");
+        }
+        Console.WriteLine("]");
+    }
+}
+
+void ReverseFirstLastRow(int[,] matrix)
+{
+    int temp = default;
+    for (int j = 0; j < matrix.GetLength(0); j++)
+    {
+
+        temp = matrix[0, j];
+        matrix[0, j] = matrix[matrix.GetLength(0) - 1, j];
+        matrix[matrix.GetLength(0) - 1, j] = temp;
+    }
+}
+
+int[,] array2d = CreateMatrixRndInt(4, 4, 0, 9);
+PrintMatrix(array2d);
+ReverseFirstLastRow(array2d);
+Console.WriteLine();
+PrintMatrix(array2d);
---
+++ Task055/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 55: 
+// 1. Задайте двумерный массив. Напишите программу, которая 
+// 2. заменяет строки на столбцы. 
+// 3. В случае, если это невозможно, программа должна вывести сообщение для пользователя.
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    var rnd = new Random();
+
+    for (int i = 0; i < rows/*i < matrix.GetLength(0)*/; i++) //0 rows
+    {
+        for (int j = 0; j < columns/*i < matrix.GetLength(1)*/; j++) //1 columns
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],4}, ");
+            else Console.Write($"{matrix[i, j],4}");
+        }
+        Console.WriteLine("]");
+    }
+}
